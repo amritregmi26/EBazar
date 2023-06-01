c217917b93da73d5c927c770ae55e0f27cd9b1c1
= <=>
2023-06-01 21:42:54 +0545

    first time adding the content to github

--- app/src/main/java/com/android/mbman/ebazar/MainActivity.kt
+++ app/src/main/java/com/android/mbman/ebazar/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.android.mbman.ebazar
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowInsetsControllerCompat
 
+
+@Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        window.decorView.systemUiVisibility = (WindowManager.LayoutParams.FLAG_FULLSCREEN or WindowInsetsControllerCompat.BEHAVIOR_SHOW_BARS_BY_SWIPE)
+        setContentView(R.layout.login)
+
     }
 }
